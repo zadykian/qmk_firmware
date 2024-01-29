61a9e1b20a95e045e1fe2a1e1a7adbc0ecf17d4a
zadykian <zadykian@outlook.com>
2024-01-29 13:34:42 +0400

    added double-tap bindings for F14-F19

--- keyboards/keebio/bdn9/keymaps/two-encoders/keymap.c
+++ keyboards/keebio/bdn9/keymaps/two-encoders/keymap.c
@@ -8,13 +8,19 @@ enum encoder_names {
 
 enum tap_dance_names {
     MEDIA_CONTROL,
+    F14_MACRO,
+    F15_MACRO,
+    F16_MACRO,
+    F17_MACRO,
+    F18_MACRO,
+    F19_MACRO,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        TD(MEDIA_CONTROL) , KC_MUTE , KC_NO  ,
-        KC_F14            , KC_F15  , KC_F16 ,
-        KC_F17            , KC_F18  , KC_F19
+        TD(MEDIA_CONTROL) , KC_MUTE       , KC_NO         ,
+        TD(F14_MACRO)     , TD(F15_MACRO) , TD(F16_MACRO) ,
+        TD(F17_MACRO)     , TD(F18_MACRO) , TD(F19_MACRO) ,
     )
 };
 
@@ -61,4 +67,12 @@ void media_control_handler(tap_dance_state_t *state, void *user_data) {
 
 tap_dance_action_t tap_dance_actions[] = {
     [MEDIA_CONTROL] = ACTION_TAP_DANCE_FN(media_control_handler),
+
+    [F14_MACRO_KEY] = ACTION_TAP_DANCE_DOUBLE(KC_F14, RCTL(KC_F14)),
+    [F15_MACRO_KEY] = ACTION_TAP_DANCE_DOUBLE(KC_F14, RCTL(KC_F15)),
+    [F16_MACRO_KEY] = ACTION_TAP_DANCE_DOUBLE(KC_F14, RCTL(KC_F16)),
+
+    [F17_MACRO_KEY] = ACTION_TAP_DANCE_DOUBLE(KC_F14, RCTL(KC_F17)),
+    [F18_MACRO_KEY] = ACTION_TAP_DANCE_DOUBLE(KC_F14, RCTL(KC_F18)),
+    [F19_MACRO_KEY] = ACTION_TAP_DANCE_DOUBLE(KC_F14, RCTL(KC_F19)),
 };
