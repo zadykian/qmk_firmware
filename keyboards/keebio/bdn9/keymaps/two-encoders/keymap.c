f782d97f1c692c075824979f4ec2668f631e3384
zadykian <zadykian@outlook.com>
2024-01-29 22:59:55 +0400

    complex bindings for F14-F19 (renaming)

--- keyboards/keebio/bdn9/keymaps/two-encoders/keymap.c
+++ keyboards/keebio/bdn9/keymaps/two-encoders/keymap.c
@@ -1,9 +1,8 @@
 #include QMK_KEYBOARD_H
 
 enum encoder_names {
-  LEFT,
-  RIGHT,
-  MIDDLE,
+    ENCODER_MIDDLE = 1,
+    ENCODER_RIGHT  = 2,
 };
 
 enum tap_dance_action_type {
@@ -13,17 +12,17 @@ enum tap_dance_action_type {
     TD_DOUBLE_TAP,
     TD_DOUBLE_HOLD,
     TD_TRIPLE_TAP,
-    TD_TRIPLE_HOLD
+    TD_TRIPLE_HOLD,
 };
 
 enum macro_pad_key_names {
     ANY_KEY,
-    F14_KEY,
-    F15_KEY,
-    F16_KEY,
-    F17_KEY,
-    F18_KEY,
-    F19_KEY
+    F04_KEY,
+    F05_KEY,
+    F06_KEY,
+    F07_KEY,
+    F08_KEY,
+    F09_KEY,
 };
 
 typedef struct {
@@ -38,20 +37,20 @@ typedef struct {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         TD(ANY_KEY) , KC_MUTE     , KC_NO       ,
-        TD(F14_KEY) , TD(F15_KEY) , TD(F16_KEY) ,
-        TD(F17_KEY) , TD(F18_KEY) , TD(F19_KEY)
+        TD(F04_KEY) , TD(F05_KEY) , TD(F06_KEY) ,
+        TD(F07_KEY) , TD(F08_KEY) , TD(F09_KEY)
     )
 };
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == MIDDLE) {
+    if (index == ENCODER_MIDDLE) {
         if (clockwise) {
             tap_code(KC_VOLU);
         } else {
             tap_code(KC_VOLD);
         }
     }
-    else if (index == RIGHT) {
+    else if (index == ENCODER_RIGHT) {
         if (clockwise) {
             tap_code(KC_NO);
         } else {
@@ -100,17 +99,16 @@ void tap_dance_on_reset(tap_dance_state_t *state, void *user_data) {
 #define TAP_DANCING(st, sh, dt, dh, tt, th) \
     { .fn = {NULL, tap_dance_on_finished, tap_dance_on_reset, NULL}, .user_data = (void *)&((tap_dance_tap_hold_t){st, sh, dt, dh, tt, th}), }
 
-
 tap_dance_action_t tap_dance_actions[] = {
     // Key    \   Action   |    1x Tap    |   1x Hold    |    2x Tap    |   2x Hold    |    3x Tap    |   3x Hold    |
 
     [ANY_KEY] = TAP_DANCING(   KC_MPLY    ,    KC_NO     ,   KC_MNXT    ,    KC_NO     ,   KC_MPRV    ,    KC_NO     ),
 
-    [F14_KEY] = TAP_DANCING(  A(KC_F14)   ,  C(KC_F14)   ,  S(KC_F14)   , A(C(KC_F14)) , A(S(KC_F14)) , C(S(KC_F14)) ),
-    [F15_KEY] = TAP_DANCING(  A(KC_F15)   ,  C(KC_F15)   ,  S(KC_F15)   , A(C(KC_F15)) , A(S(KC_F15)) , C(S(KC_F15)) ),
-    [F16_KEY] = TAP_DANCING(  A(KC_F16)   ,  C(KC_F16)   ,  S(KC_F16)   , A(C(KC_F16)) , A(S(KC_F16)) , C(S(KC_F16)) ),
+    [F04_KEY] = TAP_DANCING(  A(KC_F14)   ,  C(KC_F14)   ,  S(KC_F14)   , A(C(KC_F14)) , A(S(KC_F14)) , C(S(KC_F14)) ),
+    [F05_KEY] = TAP_DANCING(  A(KC_F15)   ,  C(KC_F15)   ,  S(KC_F15)   , A(C(KC_F15)) , A(S(KC_F15)) , C(S(KC_F15)) ),
+    [F06_KEY] = TAP_DANCING(  A(KC_F16)   ,  C(KC_F16)   ,  S(KC_F16)   , A(C(KC_F16)) , A(S(KC_F16)) , C(S(KC_F16)) ),
 
-    [F17_KEY] = TAP_DANCING(  A(KC_F17)   ,  C(KC_F17)   ,  S(KC_F17)   , A(C(KC_F17)) , A(S(KC_F17)) , C(S(KC_F17)) ),
-    [F18_KEY] = TAP_DANCING(  A(KC_F18)   ,  C(KC_F18)   ,  S(KC_F18)   , A(C(KC_F18)) , A(S(KC_F18)) , C(S(KC_F18)) ),
-    [F19_KEY] = TAP_DANCING(  A(KC_F19)   ,  C(KC_F19)   ,  S(KC_F19)   , A(C(KC_F19)) , A(S(KC_F19)) , C(S(KC_F19)) ),
+    [F07_KEY] = TAP_DANCING(  A(KC_F17)   ,  C(KC_F17)   ,  S(KC_F17)   , A(C(KC_F17)) , A(S(KC_F17)) , C(S(KC_F17)) ),
+    [F08_KEY] = TAP_DANCING(  A(KC_F18)   ,  C(KC_F18)   ,  S(KC_F18)   , A(C(KC_F18)) , A(S(KC_F18)) , C(S(KC_F18)) ),
+    [F09_KEY] = TAP_DANCING(  A(KC_F19)   ,  C(KC_F19)   ,  S(KC_F19)   , A(C(KC_F19)) , A(S(KC_F19)) , C(S(KC_F19)) ),
 };
