2e5e7a50474ee3778203f6eeb8877f7fc1a4199b
zadykian <zadykian@outlook.com>
2024-10-13 15:42:36 +0400

    defined custom keymap for mode tempo

---
+++ keyboards/mode/m60h/keymaps/layered/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//#ifdef RGB_MATRIX_ENABLE
+//#    define RGB_MATRIX_KEYPRESSES
+//#endif
+
+#define RGBLIGHT_DEFAULT_ON false
+#define BACKLIGHT_DEFAULT_ON false
+
+//#define TAPPING_TERM 250
